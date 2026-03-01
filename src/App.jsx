9280af5252bae574de28969bc33cc0be212be838
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2026-03-01 14:11:10 +0100

    model and lighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-    return <main>
+    return (<main>
         <Navbar />
         <Hero />
-    </main>;
+
+        <ProductViewer />
+    </main>);
 }
 
 export default App;
